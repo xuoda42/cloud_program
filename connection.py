0f01b4dbe8edd6be396b12419150c5406387904a
Semyon Makhalin <sem.mahalin@yandex.ru>
2023-11-28 13:18:20 +0300

    feat: second lab

--- connection.py
+++ connection.py
@@ -1,14 +1,16 @@
 import psycopg2
 
-conn = psycopg2.connect("""
-    host=rc1b-1axp7jy9wejutqav.mdb.yandexcloud.net
-    port=6432
-    sslmode=verify-full
-    dbname=db1
-    user=user1
-    password=152jojo152
-    target_session_attrs=read-write
-""")
+# conn = psycopg2.connect("""
+#     host=rc1b-1axp7jy9wejutqav.mdb.yandexcloud.net
+#     port=6432
+#     sslmode=verify-full
+#     dbname=db1
+#     user=user1
+#     password=152jojo152
+#     target_session_attrs=read-write
+# """)
+
+conn2 = psycopg2.connect("host=185.86.145.31 dbname=PostgreSQL-2483 user=user password=7M3YHV7+RA2Gh8373")
 
 
 def get_cursor_columns(cursor):
